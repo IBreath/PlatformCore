2cd05a5e8a6073036946fe02bf37f34dd1509222
Pierre-Malherbe <pierre.malherbe@sylpheo.com>
2017-10-17 10:56:16 +0200

    first commit

---
+++ src/main/java/com/ibreath/IbreathApplication.java
@@ -0,0 +1,12 @@
+package com.ibreath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbreathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbreathApplication.class, args);
+	}
+}
