454c337773ad48245ca61a01390434977bf4d48d
theobeuze <theo.beuze@sylpheo.com>
2018-06-11 16:45:09 +0200

    [FIX] api in path

--- src/main/java/com/ibreath/controller/UserController.java
+++ src/main/java/com/ibreath/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping(value = "/api")
 @Api("User Controller")
 public class UserController {
 
