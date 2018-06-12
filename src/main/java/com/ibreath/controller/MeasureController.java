62dfd82e4b0873193e92632850bac3d83aaa2239
theobeuze <theo.beuze@sylpheo.com>
2018-06-12 10:24:34 +0200

    [FIX] Path problem

--- src/main/java/com/ibreath/controller/MeasureController.java
+++ src/main/java/com/ibreath/controller/MeasureController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/measures")
+@RequestMapping(value = "/measures")
 @Api("Measure")
 public class MeasureController {
 
