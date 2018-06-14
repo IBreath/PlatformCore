a289898118f6542000f93641652015adb7c7523c
theobeuze <theo.beuze@sylpheo.com>
2018-06-14 14:55:08 +0200

    New EP : Login an user

--- src/main/java/com/ibreath/resource/model/repository/UserRepository.java
+++ src/main/java/com/ibreath/resource/model/repository/UserRepository.java
@@ -2,10 +2,10 @@ package com.ibreath.resource.model.repository;
 
 import com.ibreath.resource.model.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository  extends CrudRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
+    UserEntity findByEmailAndPassword(String email, String password);
 }
